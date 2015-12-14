fceb92073fce0d7bfe7293fb7270be26b65760e5
bensoltoff <ben@bensoltoff.com>
2015-12-14 13:40:38 -0500

    Add indexes to tables for faster queries

--- R/get_data.R
+++ R/get_data.R
@@ -14,10 +14,36 @@ load_data <- function(){
   files <- sub("^([^.]*).*", "\\1", files)
   files <- tolower(files)
   
+  ## get indexes
+  indexes <- list(block = list("pid"),
+                  chart = list("pid"),
+                  conv = list("pid"),
+                  defense = list("uid", "gid", "player"),
+                  drive = list("uid", "gid", "fpid", "tname"),
+                  fgxp = list("pid", "fgxp"),
+                  fumble = list("pid", "fum"),
+                  game = list("gid", "seas"),
+                  intercpt = list("pid", "psr", "ints"),
+                  kicker = list("uid", "gid", "player"),
+                  koff = list("uid", "kicker"),
+                  offense = list("uid", "gid", "player"),
+                  oline = list("olid"),
+                  pass = list("pid", "psr", "trg"),
+                  penalty = list("uid", "pid"),
+                  play = list("pid", "gid"),
+                  player = list("player", "fname", "lname"),
+                  punt = list("pid", "punter"),
+                  redzone = list("uid", "gid", "player"),
+                  rush = list("pid", "bc"),
+                  sack = list("uid", "pid", "qb", "sk"),
+                  safety = list("pid", "saf"),
+                  tackle = list("uid", "pid", "tck"),
+                  team = list("tid", "gid", "tname"))
+  
   ## create and fill tables
   for(name in files){
     path <- paste0("./data/armchair_analysis/", toupper(name), ".csv")
-    copy_to(my_db, read_csv(path), name = name, temporary = FALSE)
+    copy_to(my_db, read_csv(path), name = name, temporary = FALSE, indexes = indexes$`name`)
   }
   
   return(my_db)
