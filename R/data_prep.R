9b2078c4c1c5c3b3b9bed9e8074163c386a98ece
bensoltoff <ben@bensoltoff.com>
2015-12-16 17:14:09 -0500

    Enable magrittr piping in package

--- R/data_prep.R
+++ R/data_prep.R
@@ -3,17 +3,17 @@ load_games <- function(game_data_fname, remove_ties = FALSE){
   
   # Data from 2000 import is less reliable, omit this season
   # and use regular season games only.
-  games %<>%
+  games <- games %>%
     filter(seas >= 2001 & wk <= 17) %>%
     select(-stad, -temp, -humd, -wspd, -wdir, -cond, -surf)
   
   # calculate winner
-  games %<>%
+  games <- games %>%
     mutate(winner = ifelse(ptsv > ptsh, v,
                            ifelse(ptsh > ptsv, h, "TIE")))
   
   if(remove_ties){
-    games %<>%
+    games <- games %>%
       filter(winner != "TIE")
   }
   
