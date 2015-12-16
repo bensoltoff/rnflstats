5bdd89812d0ae4ba3cf7bce0a7151a59dc823ac9
bensoltoff <ben@bensoltoff.com>
2015-12-16 17:06:49 -0500

    Begin conversion of NYT 4th Down Bot to R

---
+++ R/data_prep.R
@@ -0,0 +1,22 @@
+load_games <- function(game_data_fname, remove_ties = FALSE){
+  games <- readr::read_csv(game_data_fname)
+  
+  # Data from 2000 import is less reliable, omit this season
+  # and use regular season games only.
+  games %<>%
+    filter(seas >= 2001 & wk <= 17) %>%
+    select(-stad, -temp, -humd, -wspd, -wdir, -cond, -surf)
+  
+  # calculate winner
+  games %<>%
+    mutate(winner = ifelse(ptsv > ptsh, v,
+                           ifelse(ptsh > ptsv, h, "TIE")))
+  
+  if(remove_ties){
+    games %<>%
+      filter(winner != "TIE")
+  }
+  
+  return(games)
+}
+
